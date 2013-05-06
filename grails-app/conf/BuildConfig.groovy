9c9064a399bbb9d3cd27270484d10d0dc8eccd29
Michael Ruhwedel <Michael.Ruhwedel@immobilienscout24.de>
2013-05-06 15:29:50 +0200

    Merged Changes from Subversion: -Mapper is switchable -Resources are loaded from work directory (won't work in unexploded war otherwise) - Exceptions in compilation are re-thrown to indicate errors

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,67 +1,55 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+grails.project.repos.nexus.url = "http://devnex.rz.is24.loc/content/repositories/i-search-releases"
+grails.project.repos.artifactory.url = "http://deviar01.dev.is24.loc/artifactory/simple/libs-release-local"
+grails.project.repos.default = "artifactory"
 grails.release.scm.enabled = false
-grails.project.repos.default = "grailsCentral"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+// !!! ATTENTION - Do NOT remove the quotation marks because variable replacing does not work then anymore !!!
+grails.project.war.file = "target/${appName}.war"
+grails.server.port.http = 9010
+
+coverage {
+  enabledByDefault = false
+  xml = true
+}
+
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+  checksums false
+  pom true
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        mavenCentral()
-        grailsRepo "http://grails.org/plugins"
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        runtime 'org.lesscss:lesscss:1.3.3'
+  // inherit Grails' default dependencies
+  inherits("global") {
+  }
 
-        test ("org.codehaus.geb:geb-spock:0.7.0") {
-            export = false
-        }
-        test ('org.gmock:gmock:0.8.2') {
-            export = false
-        }
-        test("org.seleniumhq.selenium:selenium-firefox-driver:2.31.0") {
-            exclude 'selenium-server'
-            export = false
-        }
+  log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
+  repositories {
+    inherit false
+    mavenRepo name: "artifactory", root: "http://deviar01.dev.is24.loc/artifactory/all"
+  }
+
+  dependencies {
+    // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+    runtime 'org.lesscss:lesscss:1.3.3'
+    test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+    test ("org.codehaus.geb:geb-spock:0.7.0") {
+      export = false
     }
-    plugins {
-        test (":spock:0.6") {
-            export = false
-        }
-        test (":geb:0.7.2") {
-            export = false
-        }
-        compile (":resources:1.1.6") {
-            export = false
-        }
-        compile (":hibernate:2.0.0") {
-            export = false
-        }
-        compile (":rest-client-builder:1.0.2") {
-            export = false
-        }
-        compile (":tomcat:2.0.0") {
-            export = false
-        }
-//        build(':release:2.0.4', ':rest-client-builder:1.0.2') {
-//           export = false
-//        }
+    test ('org.gmock:gmock:0.8.2') {
+      export = false
     }
+  }
+
+  plugins {
+    compile(':resources:1.2.RC2') { export = false }
+    build(":release:$grailsVersion") { export = false }
+    test(":spock:0.7") {
+      exclude "spock-grails-support"
+    }
+    compile (":tomcat:$grailsVersion") { export = false }
+  }
 }
--- grails-app/resourceMappers/LesscssResourceMapper.groovy
+++ grails-app/resourceMappers/LesscssResourceMapper.groovy
@@ -12,47 +12,47 @@ import org.lesscss.LessException
 
 class LesscssResourceMapper implements GrailsApplicationAware {
 
-    GrailsApplication grailsApplication
-    LessCompiler lessCompiler
+  GrailsApplication grailsApplication
+  LessCompiler lessCompiler
 
-    def phase = MapperPhase.GENERATION // need to run early so that we don't miss out on all the good stuff
+  def phase = MapperPhase.GENERATION // need to run early so that we don't miss out on all the good stuff
 
-    static defaultIncludes = ['**/*.less']
+  static defaultIncludes = ['**/*.less']
 
-    def map(resource, config) {
-        if(!lessCompiler) {
-            lessCompiler = new LessCompiler()
-            lessCompiler.setCompress(grailsApplication.config.grails?.resources?.mappers?.lesscss?.compress == true ?: false)
-        }
-        File originalFile = resource.processedFile
-        File input = getOriginalFileSystemFile(resource.sourceUrl);
-        File target = new File(generateCompiledFileFromOriginal(originalFile.absolutePath))
-
-        if (log.debugEnabled) {
-            log.debug "Compiling LESS file [${originalFile}] into [${target}], with compress [${grailsApplication.config.grails?.resources?.mappers?.lesscss?.compress}]"
-        }
-        try {
-            lessCompiler.compile input, target
-            // Update mapping entry
-            // We need to reference the new css file from now on
-            resource.processedFile = target
-            // Not sure if i really need these
-            resource.sourceUrlExtension = 'css'
-            resource.contentType = 'text/css'
-            resource.tagAttributes?.rel = 'stylesheet'
-            resource.updateActualUrlFromProcessedFile()
-
-        } catch (LessException e) {
-            log.error("error compiling less file: ${originalFile}", e)
-        }
-
+  def map(resource, config) {
+    if (grailsApplication.config.grails?.resources?.mappers?.lesscss?.disabled) {
+      return
     }
 
-    private String generateCompiledFileFromOriginal(String original) {
-         original + '.css'
+    if (!lessCompiler) {
+      lessCompiler = new LessCompiler()
+      lessCompiler.compress = grailsApplication.config.grails?.resources?.mappers?.lesscss?.compress == true ?: false
     }
+    File originalFile = resource.processedFile
+    File input = new File(resource.workDir, resource.sourceUrl);
+    File target = new File(generateCompiledFileFromOriginal(originalFile.absolutePath))
 
-    private File getOriginalFileSystemFile(String sourcePath) {
-        grailsApplication.parentContext.getResource(sourcePath).file
+    if (log.debugEnabled) {
+      log.debug "Compiling LESS file [${originalFile}] into [${target}], with compress [${grailsApplication.config.grails?.resources?.mappers?.lesscss?.compress}]"
     }
+    try {
+      lessCompiler.compile input, target
+      // Update mapping entry
+      // We need to reference the new css file from now on
+      resource.processedFile = target
+      // Not sure if i really need these
+      resource.sourceUrlExtension = 'css'
+      resource.contentType = 'text/css'
+      resource.tagAttributes?.rel = 'stylesheet'
+      resource.updateActualUrlFromProcessedFile()
+
+    } catch (LessException e) {
+      log.error("error compiling less file: ${originalFile}", e)
+      throw e;
+    }
+  }
+
+  private String generateCompiledFileFromOriginal(String original) {
+    original + '.css'
+  }
 }
